e40758f5b81e15a685c09d27a770364eb80aad2c
sangyun lee <sororiri@gmail.com>
2024-07-14 22:05:56 +0900

    Add: styled Compoment Library  사용 예시 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
 import Button from "./Button";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20vh;
+`;
+
+interface TextProps {
+  $bgColor: string;
+}
+
+interface RadiusTextProps extends TextProps {
+  $radiusPx?: string;
+}
+
+const Text = styled.div<TextProps>`
+  background-color: ${(props) => props.$bgColor};
+  font-size: 2rem;
+  height: 50px;
+`;
+
+const RadiusText = styled(Text)<RadiusTextProps>`
+  border-radius: ${(props) => props.$radiusPx || '10px'};
+`;
+
+const Input = styled.input.attrs({
+  type: 'password',
+})<TextProps>
+  `
+  background-color: ${(props) => props.$bgColor};
+  font-size: 2rem;
+  height: 50px;
+`;
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <h1>웃꾜 ... </h1>
+      <Container>
+      <Text $bgColor="tomato">Text </Text>
+      <RadiusText $bgColor="gray" $radiusPx="15px">RadiusText </RadiusText>
+      </Container>
+      <Input $bgColor="lightblue" />
       <Routes>
         <Route path="/btn/:id" element={<Button />} />
       </Routes>
