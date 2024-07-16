81ecd46eadc61526cbb4744818fc0fc0191bdf06
sangyun lee <sororiri@gmail.com>
2024-07-16 21:52:59 +0900

    Add: Header 에 로그인 버튼 추가

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -9,9 +9,34 @@ const Container = styled.div`
   background-color: ${(props) => props.theme.headerbgColor};
   color: ${(props) => props.theme.headerTextColor};
   border: 1px solid black;
+  padding: 10px 10px;
 `;
 
+const Title = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const Action = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-left: auto;
+`
+const ActionButton = styled.button`
+  padding: 5px 5px;
+  font-size: 15px;
+  cursor: pointer;
+`
+
 function Header() {
-  return ( <Container>이건 뭘까</Container> );
+  return ( <Container>
+    <Title>타이틀</Title>
+    <Action>
+        <ActionButton>Login</ActionButton>
+    </Action>
+  </Container> );
 }
 export default Header;
