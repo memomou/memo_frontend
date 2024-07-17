36637e9070002edff9d279e903fd06c7d743afeb
sangyun lee <sororiri@gmail.com>
2024-07-17 23:36:51 +0900

    Add: Page 별 폴더 구분

--- src/views/Body.tsx
+++ src/views/Body.tsx
@@ -1,18 +1,28 @@
 import styled from "styled-components";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
+import Form from "../components/Form";
+import { RadiusText } from "../components/Text";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Root from "./pages/Root";
 
 const Container = styled.div`
   display: flex;
   height: calc(100vh - 160px);
   min-height: 450px;
+
+  border-left: 1px solid black;
+  border-right: 1px solid black;
 `;
 
 function Body() {
   return (
     <Container>
-      <Sidebar />
-      <Content />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root/>} />
+        </Routes>
+      </Router>
     </Container>
   );
 }
---
+++ src/views/pages/Root.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import Content from "../Content";
+
+const Styled = styled.div`
+  flex: 1;
+`;
+
+function Root() {
+  return (
+    <Styled>
+      <Content />
+    </Styled>
+  );
+}
+
+export default Root;
