cbed8d1e5f4f271eedb29f67ca1526c7de0d4e2c
sangyun lee <sororiri@gmail.com>
2024-08-26 17:01:40 +0900

    사이트 아이콘 배경 추가

--- src/views/pages/root/Content.tsx
+++ src/views/pages/root/Content.tsx
@@ -55,16 +55,9 @@ function Content() {
 
   return (
     <ContentContainer>
-      <div className="sidebarWrapper">
-        <Link to="/">최신 글 보기</Link>
-        <Link to="/">공지사항</Link>
-        <Link to="/">건의사항</Link>
-        {/* <div className="item">공지사항</div>
-        <div className="item">건의 투석함</div> */}
-      </div>
       <div className="recentPosterWrapper">
         <div className="topContainer">
-          <span className="recentPostText">{searchInputValue ? `- 검색 결과 (${searchedPosts.length})` : `- 자유 게시판`}</span>
+          <span className="recentPostText">{searchInputValue ? `- 검색 결과 (${searchedPosts.length})` : `- 전체 게시글`}</span>
           <span className="searchInputWrapper">
             <svg width="17" height="17" viewBox="0 0 17 17"><path fillRule="evenodd" d="M13.66 7.36a6.3 6.3 0 1 1-12.598 0 6.3 6.3 0 0 1 12.598 0zm-1.73 5.772a7.36 7.36 0 1 1 1.201-1.201l3.636 3.635c.31.31.31.815 0 1.126l-.075.075a.796.796 0 0 1-1.126 0l-3.636-3.635z" clipRule="evenodd" fill="currentColor"></path></svg>
             <input className="searchInput" type="text" placeholder="검색"       value={searchInputValue}
