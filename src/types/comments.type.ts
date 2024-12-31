2dc2a446ed1c295fedf91f550a52d2f34af47f99
sangyun lee <sororiri@gmail.com>
2024-12-31 18:00:57 +0900

    dd

--- src/types/comments.type.ts
+++ src/types/comments.type.ts
@@ -2,7 +2,9 @@ import { UserState } from "./users.type";
 
 export interface CommentType {
   id?: number;
+  postId?: number;
   content?: string;
   createdAt?: string;
+  updatedAt?: string;
   user?: UserState;
 }
